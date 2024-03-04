e2b90df2ad04f23b9563543ee318d882d13ceac6
Karan <150646218+Karan-dvlpr07@users.noreply.github.com>
2024-03-04 13:09:30 +0530

    Delete Qrcode directory

--- Qrcode/Qrcode.py
+++
@@ -1,5 +0,0 @@
-
-import qrcode
-
-X_dvlpr = qrcode.make("https://github.com/Karan-dvlpr07")
-X_dvlpr.save("Git.jpg")
--- Qrcode/Qrcode2.py
+++
@@ -1,6 +0,0 @@
-
-import pyqrcode
-
-url = "https://github.com/Karan-dvlpr07"
-X_Dvlpr = pyqrcode.create(url)
-X_Dvlpr.svg("Git.Svg",scale=10)
